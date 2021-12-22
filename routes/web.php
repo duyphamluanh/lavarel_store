5db0b2f9641fad579e538c3718dc1ff16f82a713
duyphamluanh <anhduy123000@gmail.com>
2021-12-22 22:12:29 +0700

    get

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\Admin\Users\MainController;
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/admin/main', [ MainController::class , 'index'])->name('admin');
+Route::get('/admin/users/login', [ LoginController::class , 'index'])->name('login');
+Route::post('/admin/users/login/store', [ LoginController::class , 'store']);
